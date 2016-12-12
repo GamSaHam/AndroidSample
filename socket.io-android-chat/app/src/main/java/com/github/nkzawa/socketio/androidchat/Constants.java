25383345c0205ad72141cf621218706ed0743484
GamSaHam <cleverones@naver.com>
2016-12-12 11:47:09 +0900

    add sample socket.io

---
+++ socket.io-android-chat/app/src/main/java/com/github/nkzawa/socketio/androidchat/Constants.java
@@ -0,0 +1,5 @@
+package com.github.nkzawa.socketio.androidchat;
+
+public class Constants {
+    public static final String CHAT_SERVER_URL = "http://ec2-52-78-204-8.ap-northeast-2.compute.amazonaws.com:9000";
+}
---
+++ socket.io-android-chat/app/src/main/java/com/github/nkzawa/socketio/androidchat/MainFragment.java
@@ -0,0 +1,115 @@
+package com.github.nkzawa.socketio.androidchat;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+/**
+ * A chat fragment containing messages view and input form.
+ */
+public class MainFragment extends Fragment
+{
+    private RecyclerView mMessagesView;
+
+    private RecyclerView.Adapter mAdapter;
+
+    private Socket mSocket;
+
+
+    public MainFragment() {
+        super();
+    }
+
+    @Override
+    public void onAttach(Activity activity)
+    {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setHasOptionsMenu(true);
+
+        ChatApplication app = (ChatApplication) getActivity().getApplication();
+        mSocket = app.getSocket();
+
+        mSocket.on(Socket.EVENT_CONNECT,onConnect);
+        mSocket.on(Socket.EVENT_DISCONNECT,onDisconnect);
+        mSocket.on(Socket.EVENT_CONNECT_ERROR, onConnectError);
+        mSocket.on(Socket.EVENT_CONNECT_TIMEOUT, onConnectError);
+
+        mSocket.connect();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mMessagesView = (RecyclerView) view.findViewById(R.id.messages);
+        mMessagesView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mMessagesView.setAdapter(mAdapter);
+
+    }
+
+
+
+    private Emitter.Listener onConnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener onDisconnect = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run()
+                {
+
+                  //  Toast.makeText(getActivity().getApplicationContext(),
+                         //   R.string.disconnect, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    };
+
+    private Emitter.Listener onConnectError = new Emitter.Listener() {
+        @Override
+        public void call(Object... args) {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                   // Toast.makeText(getActivity().getApplicationContext(),
+                         //   R.string.error_connect, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    };
+
+}
+
